2c9d8db22d095bfbb99044ac9636235f4727bf07
Filipe Esteves <filipe.esteves08@gmail.com>
2018-10-22 18:14:11 -0300

    Mudança na barra de navegação

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import './styles.css';
 
 const Header = () => (
-        <header id="main-header">CurriculumElkas</header>
+        <header id="main-header">
+        
+        <nav class="navbar navbar-light bg-light justify-content-center">
+            <span className="navbar-brand mb-0 h1 navegacao">CurriculumElkas</span>
+        </nav>
+        
+        </header>
     );    
 
 export default Header;
