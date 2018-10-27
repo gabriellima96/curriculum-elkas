9ee15ee9b1a71003dec4eca27372397549711567
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-10-27 15:55:42 -0300

    Adicionando o model de user

--- curriculum-elkas-backend/config/database.js
+++ curriculum-elkas-backend/config/database.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 module.exports = {
   url: 'mongodb://localhost:27017/curriculumelkasdb',
-  config: { useNewUrlParser: true },
+  config: { useNewUrlParser: true, useCreateIndex: true },
+  modelPath: path.resolve('app', 'models'),
 };
--- curriculum-elkas-backend/index.js
+++ curriculum-elkas-backend/index.js
@@ -1,12 +1,15 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 
+const requireDir = require('require-dir');
 const configDb = require('./config/database');
 
 mongoose.connect(
   configDb.url,
   configDb.config,
 );
+
+requireDir(configDb.modelPath);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
