b0ff691d27474d96b436a8304fa3b4df937ef1e8
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-10-28 02:01:09 -0300

    Revert "Testando CI"
    This reverts commit dc57c81fd83c4ec33b87d7497ee9e2b8ff836ef7.

--- curriculum-elkas-backend/app/routes.js
+++ curriculum-elkas-backend/app/routes.js
@@ -20,6 +20,6 @@ router.post('/signin', controllers.authController.signin);
  */
 // route.get('/:id/informations', controllers.userController.index);
 
-router.get('/test', (req, res) => res.send('CI success 3'));
+router.get('/test', (req, res) => res.send('CI success'));
 
 module.exports = router;
