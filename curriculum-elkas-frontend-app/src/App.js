3028b8045d665ea73586968d7cd071efbc1152c3
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-10-28 22:21:53 -0300

    Ajustando o materialize

--- curriculum-elkas-frontend-app/src/App.js
+++ curriculum-elkas-frontend-app/src/App.js
@@ -3,6 +3,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import Content from './pages/main';
 
+import 'font-awesome/css/font-awesome.css';
+
 library.add(faSignInAlt, faUserPlus);
 
 const App = () => (
