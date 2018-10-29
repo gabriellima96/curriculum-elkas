96ddc1757a797ea1ae57ddb9027820c1e0159136
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-10-28 23:11:17 -0300

    Ajustes nos cards

--- curriculum-elkas-frontend-app/src/App.js
+++ curriculum-elkas-frontend-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
 import Content from './pages/main';
+import './styles/styles.css';
 
 library.add(faSignInAlt, faUserPlus);
 
