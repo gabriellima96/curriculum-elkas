5e59b4dca37f9b3e15a9be14c5d15d0b06cc2755
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-02 15:55:52 -0300

    Adicionando o loading e atualizando a rota de signup

--- curriculum-elkas-frontend-app/src/components/SignUp/index.js
+++ curriculum-elkas-frontend-app/src/components/SignUp/index.js
@@ -10,6 +10,7 @@ class SignUp extends Component {
     password: '',
     confirmPassword: '',
     error: '',
+    loading: false,
   };
 
   handleSignUp = async (e) => {
@@ -23,7 +24,8 @@ class SignUp extends Component {
       this.setState({ error: 'As senhas não conferem' });
     } else {
       try {
-        const response = await api.post('/signup', {
+        this.setState({ loading: true });
+        const response = await api.post('/users', {
           name,
           username,
           email,
@@ -32,86 +34,95 @@ class SignUp extends Component {
 
         console.log(response);
         console.log('criado com sucesso');
+        this.setState({ error: '' });
       } catch (error) {
         this.setState({ error: error.response.data.error });
+      } finally {
+        this.setState({ loading: false });
       }
     }
   };
 
   render() {
-    const { error } = this.state;
+    const { error, loading } = this.state;
 
     return (
-          <form onSubmit={this.handleSignUp}>
-            <h4><i class="material-icons">view_agenda</i> Cadastrar</h4>
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  id="name_inline"
-                  type="text"
-                  className="validate"
-                  onChange={e => this.setState({ name: e.target.value })}
-                />
-                <label htmlFor="name_inline">Nome completo</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  id="username_inline"
-                  type="text"
-                  className="validate"
-                  onChange={e => this.setState({ username: e.target.value })}
-                />
-                <label htmlFor="username_inline">Nome de usuário</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  id="email_inline"
-                  type="email"
-                  className="validate"
-                  onChange={e => this.setState({ email: e.target.value })}
-                />
-                <label htmlFor="email_inline">Email</label>
-                <span className="helper-text" data-error="Algo está errado!" data-success="Ok.">
-                </span>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  id="password"
-                  type="password"
-                  className="validate"
-                  onChange={e => this.setState({ password: e.target.value })}
-                />
-                <label htmlFor="password">Senha</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input
-                  id="confirmPassword"
-                  type="password"
-                  className="validate"
-                  onChange={e => this.setState({ confirmPassword: e.target.value })}
-                />
-                <label htmlFor="confirmPassword">Confirmar senha</label>
-              </div>
-            </div>
-           
-          <div className=" center-align">
-            <button className="waves-effect waves-light btn indigo" type="submit" name="action">
-              Cadastrar 
-              {' '}
-              <FontAwesomeIcon icon="user-plus" />
-            </button>
+      <form onSubmit={this.handleSignUp}>
+        <h4>
+          <i className="material-icons">view_agenda</i>
+          Cadastrar
+        </h4>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="name_inline"
+              type="text"
+              className="validate"
+              onChange={e => this.setState({ name: e.target.value })}
+            />
+            <label htmlFor="name_inline">Nome completo</label>
           </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="username_inline"
+              type="text"
+              className="validate"
+              onChange={e => this.setState({ username: e.target.value })}
+            />
+            <label htmlFor="username_inline">Nome de usuário</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="email_inline"
+              type="email"
+              className="validate"
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+            <label htmlFor="email_inline">Email</label>
+            <span className="helper-text" data-error="Algo está errado!" data-success="Ok." />
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="password"
+              type="password"
+              className="validate"
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+            <label htmlFor="password">Senha</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input
+              id="confirmPassword"
+              type="password"
+              className="validate"
+              onChange={e => this.setState({ confirmPassword: e.target.value })}
+            />
+            <label htmlFor="confirmPassword">Confirmar senha</label>
+          </div>
+        </div>
 
-            {error && <p className="center-align red-text">{error}</p>}
-          </form>
+        <div className=" center-align">
+          <button className="waves-effect waves-light btn indigo" type="submit" name="action">
+            Cadastrar
+            {' '}
+            {!loading ? (
+              <FontAwesomeIcon icon="user-plus" />
+            ) : (
+              <i className="fa fa-spinner fa-pulse" />
+            )}
+          </button>
+        </div>
+
+        {error && <p className="center-align red-text">{error}</p>}
+      </form>
     );
   }
 }
--- curriculum-elkas-frontend-app/src/pages/main/index.js
+++ curriculum-elkas-frontend-app/src/pages/main/index.js
@@ -2,25 +2,32 @@ import React, { Fragment } from 'react';
 import SignUp from '../../components/SignUp';
 import SignIn from '../../components/SignIn';
 import Header from '../../components/Header';
+import 'font-awesome/css/font-awesome.css';
 import './styles.css';
 
 const Main = () => (
-  <Fragment>    
-    <Header /> 
-    <div className="container">    
+  <Fragment>
+    <Header />
+    <div className="container">
       <div className="row">
-        <div class="col m7 push-m3 collap">
-          <ul class="collapsible popout">
+        <div className="col m7 push-m3 collap">
+          <ul className="collapsible popout">
             <li>
-              <div class="collapsible-header"><i class="material-icons">input</i>Entrar</div>
-              <div class="collapsible-body">
-              <SignIn /> 
+              <div className="collapsible-header">
+                <i className="material-icons">input</i>
+                Entrar
+              </div>
+              <div className="collapsible-body">
+                <SignIn />
               </div>
             </li>
             <li>
-              <div class="collapsible-header"><i class="material-icons">view_agenda</i>Cadastrar</div>
-              <div class="collapsible-body">
-              <SignUp />
+              <div className="collapsible-header">
+                <i className="material-icons">view_agenda</i>
+                Cadastrar
+              </div>
+              <div className="collapsible-body">
+                <SignUp />
               </div>
             </li>
           </ul>
