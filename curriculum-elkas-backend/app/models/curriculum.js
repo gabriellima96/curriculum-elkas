1b5f6d8117fc622519bbc7659cdfd66dd8bdc79b
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-03 20:53:22 -0300

    Adicionando o model de curriculum

---
+++ curriculum-elkas-backend/app/models/curriculum.js
@@ -0,0 +1,136 @@
+const mongoose = require('mongoose');
+
+const CurriculumSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  template: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  emails: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+  phones: [
+    {
+      type: String,
+    },
+  ],
+  dateOfBirth: {
+    type: Date,
+  },
+  maritalStatus: {
+    type: String,
+  },
+  academicDegree: [
+    {
+      institution: {
+        type: String,
+      },
+      degree: {
+        type: String,
+      },
+      course: {
+        type: String,
+      },
+      initialization: {
+        type: Date,
+      },
+      conclusion: {
+        type: Date,
+      },
+    },
+  ],
+  address: {
+    publicArea: {
+      type: String,
+    },
+    district: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    postalCode: {
+      type: String,
+    },
+    country: {
+      type: String,
+      required: false,
+      default: 'Brasil',
+    },
+    state: {
+      type: String,
+      required: false,
+    },
+  },
+  goals: {
+    type: String,
+    required: true,
+  },
+  skills: [
+    {
+      title: {
+        type: String,
+      },
+      description: {
+        type: String,
+      },
+      other: {
+        type: String,
+      },
+    },
+  ],
+  experiences: {
+    office: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    company: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+    },
+    initialization: {
+      type: Date,
+    },
+    conclusion: {
+      type: Date,
+    },
+  },
+  languages: [
+    {
+      language: {
+        type: String,
+      },
+
+      fluency: {
+        type: String,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+mongoose.model('Curriculum', CurriculumSchema);
