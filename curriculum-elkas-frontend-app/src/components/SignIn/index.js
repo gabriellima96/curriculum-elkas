e9059e084b6a3081dd85f078f6929d8af1063716
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-02 21:07:18 -0300

    Atualizando o component signin e redirencionando ele após sucesso

--- curriculum-elkas-frontend-app/src/components/SignIn/index.js
+++ curriculum-elkas-frontend-app/src/components/SignIn/index.js
@@ -1,31 +1,87 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { withRouter } from 'react-router-dom';
+import api from '../../services/api';
+import { login } from '../../services/auth';
 
-const SignIn = () => (
-      <form>
-        <h4><i class="material-icons">input</i> Entrar</h4>
+class SignIn extends Component {
+  state = {
+    email: '',
+    password: '',
+    loading: '',
+    error: '',
+  };
+
+  handleSignUp = async (e) => {
+    e.preventDefault();
+    const { email, password } = this.state;
+
+    if (!email || !password) {
+      this.setState({ error: 'Preencha todos os dados para entrar' });
+    } else {
+      try {
+        this.setState({ loading: true });
+        const response = await api.post('/users/signin', { email, password });
+        login(response.data.token);
+        const { history } = this.props;
+        history.push('/app');
+      } catch (error) {
+        this.setState({ error: error.response.data.error });
+      } finally {
+        this.setState({ loading: false });
+      }
+    }
+  };
+
+  render() {
+    const { error, loading } = this.state;
+
+    return (
+      <form onSubmit={this.handleSignUp}>
+        <h4>
+          <i className="material-icons">input</i>
+          {' '}
+Entrar
+        </h4>
         <div className="row">
           <div className="input-field col s12">
-            <input id="email_inline" type="email" className="validate" />
-            <label htmlFor="email_inline">Email</label>
-            <span className="helper-text" data-error="Algo está errado!" data-success="Ok.">
-            </span>
+            <input
+              id="email_inline_input"
+              type="email"
+              className="validate"
+              onChange={e => this.setState({ email: e.target.value })}
+            />
+            <label htmlFor="email_inline_input">Email</label>
+            <span className="helper-text" data-error="Algo está errado!" data-success="Ok." />
           </div>
         </div>
         <div className="row">
           <div className="input-field col s12">
-            <input id="password" type="password" className="validate" />
-            <label htmlFor="password">Senha</label>
+            <input
+              id="password_input"
+              type="password"
+              className="validate"
+              onChange={e => this.setState({ password: e.target.value })}
+            />
+            <label htmlFor="password_input">Senha</label>
           </div>
         </div>
         <div className=" center-align">
           <button className="waves-effect waves-light btn indigo" type="submit" name="action">
             Entrar
             {' '}
-            <FontAwesomeIcon icon="sign-in-alt" />
+            {!loading ? (
+              <FontAwesomeIcon icon="sign-in-alt" />
+            ) : (
+              <i className="fa fa-spinner fa-pulse" />
+            )}
           </button>
         </div>
-      </form>
-);
 
-export default SignIn;
+        {error && <p className="center-align red-text">{error}</p>}
+      </form>
+    );
+  }
+}
+
+export default withRouter(SignIn);
