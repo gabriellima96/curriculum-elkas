09c504e8b52b1c4fff7b30394a39f8f17f07a498
Filipe Esteves <filipe.esteves08@gmail.com>
2018-11-04 17:20:24 -0300

    Ajustando modelos de temas

--- curriculum-elkas-frontend-app/src/components/ContentTemplate/index.js
+++ curriculum-elkas-frontend-app/src/components/ContentTemplate/index.js
@@ -8,34 +8,35 @@ import "./styles.css";
 const ContentTemplate = () => (
 
     <div className="container">
+    
         <div className="row">
             <div className="col s12 m6">
             <div className="card z-depth-2">
                 <div className="card-image">
                 <div className="shadowImg">
-                <img src="https://images.pexels.com/photos/1036808/pexels-photo-1036808.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+                <img src="https://picsum.photos/450/300?image=1048"></img>
                 </div>
                 <span className="card-title">Moderno</span>
                 </div>
                 <div className="card-content">
                 <p>Neste tema, você gerará um currículo mais atual, com informações mais precisas, detalhadas e focadas ao cargo pelo qual você pretende concorrer.</p>
                 </div>
-                <div className="card-action">
-                <a href="#">Ecolher este tema</a>
+                <div className="card-action grey darken-4">
+                <a className="white-text" href="#">Ecolher este tema</a>
                 </div>
             </div>
             </div>
             <div className="col s12 m6">
             <div className="card z-depth-2">
                 <div className="card-image">
-                <img src="https://images.pexels.com/photos/1036808/pexels-photo-1036808.jpeg?auto=compress&cs=tinysrgb&h=350"></img>
+                <img src="https://picsum.photos/450/300?image=979"></img>
                 <span className="card-title">Clássico</span>
                 </div>
                 <div className="card-content">
-                <p>Aqui seu currículo será gerado com uma formatação mais popular. Com informações menos detalhadas e menos precisas você poderá utilizar para entrevistas de emprego que não necessitem de tanta especialização para o cargo.</p>
+                <p>Com informações menos detalhadas e menos precisas você poderá utilizar para entrevistas de emprego que não necessitem de tanta especialização para o cargo.</p>
                 </div>
-                <div className="card-action">
-                <a href="#">Ecolher este tema</a>
+                <div className="card-action grey darken-4">
+                <a className="white-text" href="#">Ecolher este tema</a>
                 </div>
             </div>
             </div>
