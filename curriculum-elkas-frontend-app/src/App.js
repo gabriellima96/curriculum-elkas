103f84ca0099335839a7f942361749093c4f95fd
Filipe Esteves <filipe.esteves08@gmail.com>
2018-11-04 17:24:40 -0300

    Adicionando ícones na página de templates

--- curriculum-elkas-frontend-app/src/App.js
+++ curriculum-elkas-frontend-app/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faSignInAlt, faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faSignInAlt, faUserPlus, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import './styles/styles.css';
 import Routes from './routes';
 import 'font-awesome/css/font-awesome.css';
 
-library.add(faSignInAlt, faUserPlus);
+library.add(faSignInAlt, faUserPlus, faArrowAltCircleRight);
 
 const App = () => <Routes />;
 
--- curriculum-elkas-frontend-app/src/components/ContentTemplate/index.js
+++ curriculum-elkas-frontend-app/src/components/ContentTemplate/index.js
@@ -22,7 +22,7 @@ const ContentTemplate = () => (
                 <p>Neste tema, você gerará um currículo mais atual, com informações mais precisas, detalhadas e focadas ao cargo pelo qual você pretende concorrer.</p>
                 </div>
                 <div className="card-action grey darken-4">
-                <a className="white-text" href="#">Ecolher este tema</a>
+                <a className="white-text" href="#">Ecolher este tema <FontAwesomeIcon icon="arrow-alt-circle-right" /></a>
                 </div>
             </div>
             </div>
@@ -36,7 +36,7 @@ const ContentTemplate = () => (
                 <p>Com informações menos detalhadas e menos precisas você poderá utilizar para entrevistas de emprego que não necessitem de tanta especialização para o cargo.</p>
                 </div>
                 <div className="card-action grey darken-4">
-                <a className="white-text" href="#">Ecolher este tema</a>
+                <a className="white-text" href="#">Ecolher este tema <FontAwesomeIcon icon="arrow-alt-circle-right" /></a>
                 </div>
             </div>
             </div>
