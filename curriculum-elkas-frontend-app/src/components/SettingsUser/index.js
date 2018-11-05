d3ca7cb63802fc66f79267d430678f40c36adb97
Filipe Esteves <filipe.esteves08@gmail.com>
2018-11-05 18:51:11 -0300

    Adicionando lista de emails (incompleto)

--- curriculum-elkas-frontend-app/src/components/SettingsUser/index.js
+++ curriculum-elkas-frontend-app/src/components/SettingsUser/index.js
@@ -7,22 +7,36 @@ import './styles.css';
 
 class SettingsUser extends Component {
 
-    addEmail = () => 
-        <div className="row">
-            <div className="input-field col s12">
-            <input id="email" type="email" className="validate"></input>
-            <label for="email">Email</label>
-            </div>
-        </div>;
+    constructor(props){
+        super(props);
+
+        this.state = {
+            addEmail: []
+        }
+    }
+
+    addEmails(e){
+        e.preventDefault();
+        const {addEmail} = this.state;
+        const newEmail = this.newEmail.value;
+
+        this.setState({
+            addEmail: [...this.state.addEmail, newEmail]
+        })
+
+        this.addForm.reset();
+    }
+   
 
     render() {
+        const {addEmail} = this.state;
 
         return(
             <div className="container">  
                 <div className="row titleForm">
                     <h4>Configurações da conta</h4>
                     <div className="container">
-                        <form className="col s12 formSettings">
+                        <form className="col s12 formSettings" >
                             <div className="row">
                                 <h5 className="subtitle center-align" >Informações da conta<hr></hr></h5>
                                 <div className="input-field col s12">
@@ -43,7 +57,7 @@ class SettingsUser extends Component {
                             <div className="row">
                                 <div className="input-field col s12">
                                 <input disabled value="example@example.com" id="disabled" type="text" className="validate"></input>
-                                <label for="disabled">Email</label>
+                                <label or="disabled">Email</label>
                                 </div>
                             </div>
                             <div className="row">
@@ -66,16 +80,51 @@ class SettingsUser extends Component {
                             </div>                               
                         </form>
 
-                        <form className="col s12 formSettings">
-                            {this.addEmail()}
+                        <form className="col s12 formSettings" > 
                             <div className="row">
+                                <h5 className="subtitle center-align" >Informações pessoais<hr></hr></h5>
+                                <form ref={input => this.addForm = input} onSubmit={(e) => {this.addEmails(e)}}>
+                                    <div className="input-field col s11">
+                                        <div className="row">            
+                                            <input ref={input => this.newEmail = input} id="email" type="email" className="validate email"></input>
+                                            <label for="email">Email</label>            
+                                        </div>
+
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>Emails</th>
+                                                </tr>
+                                            </thead>
+
+                                            <tbody>
+                                                {
+                                                    addEmail.map(email => {
+                                                        return (
+                                                            <tr key={email}>
+                                                                <td>{email}</td>
+                                                                <td>Deletar</td>
+                                                            </tr>
+                                                        );
+                                                    })
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div> 
+                                    <div className="col s1">            
+                                    <button className="btn-floating btn-small waves-effect waves-light purple">
+                                        <i className="material-icons">add</i>
+                                    </button>
+                                </div> 
+                                </form>
+                                
                                 <div className="input-field col s6">
-                                <input id="password" type="password" className="validate"></input>
-                                <label for="password">Nova senha</label>
+                                    <input id="password" type="password" className="validate"></input>
+                                    <label for="password">Nova senha</label>
                                 </div>
                                 <div className="input-field col s6">
-                                <input id="password" type="password" className="validate"></input>
-                                <label for="password">Repetir nova senha</label>
+                                    <input id="password" type="password" className="validate"></input>
+                                    <label for="password">Repetir nova senha</label>
                                 </div>
                             </div>
 
