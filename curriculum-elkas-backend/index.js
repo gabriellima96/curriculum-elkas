bd0b492b609b0180680f1976afd3e9039026a3e8
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-11 00:19:28 -0300

    Alterando a url da doc e porta

--- curriculum-elkas-backend/index.js
+++ curriculum-elkas-backend/index.js
@@ -1,24 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const app = require('express')();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const requireDir = require('require-dir');
-const cors = require('cors');
-const swaggerUI = require('swagger-ui-express');
-const configDb = require('./config/database');
-const swaggerDocument = require('./config/swaggerDocument');
+const app = require("express")();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const requireDir = require("require-dir");
+const cors = require("cors");
+const swaggerUI = require("swagger-ui-express");
+const configDb = require("./config/database");
+const swaggerDocument = require("./config/swaggerDocument");
 
 mongoose.connect(
   configDb.url,
-  configDb.config,
+  configDb.config
 );
 
 requireDir(configDb.modelPath);
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument.json));
-app.use('/api', require('./app/routes'));
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument.json));
+app.use("/api", require("./app/routes"));
 
-app.listen(8080);
+app.listen(3000);
