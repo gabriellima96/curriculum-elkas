6b4b316995e6cd8b80c1ce0d70a9ffd191075fb4
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-11 16:52:41 -0300

    Alterando para o novo dominio

--- curriculum-elkas-frontend-app/src/services/api.js
+++ curriculum-elkas-frontend-app/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getToken } from './auth';
 
 const api = axios.create({
-  baseURL: 'http://167.99.182.79/api',
+  baseURL: 'http://api.curriculumelkas.com/api',
 });
 
 api.interceptors.request.use(async (configObj) => {
