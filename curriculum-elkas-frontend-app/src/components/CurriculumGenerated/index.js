1626d04bc350950f1d44bb1909c2ad83bf40277d
Filipe Esteves <filipe.esteves08@gmail.com>
2018-11-11 18:51:26 -0300

    Criando CurriculumGenerated

---
+++ curriculum-elkas-frontend-app/src/components/CurriculumGenerated/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/button-has-type */
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Input } from "react-materialize";
+import api from "../../services/api";
+import { getUsername } from "../../services/auth";
+
+class CurriculumGenerated extends Component {
+  render() {
+    return (
+      <div classname="container">
+        <div className="row">Teste</div>
+      </div>
+    );
+  }
+}
+export default CurriculumGenerated;
---
+++ curriculum-elkas-frontend-app/src/pages/curriculumGenerated/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import HeaderApp from "../../components/HeaderApp";
+import CurriculumGenerated from "../../components/CurriculumGenerated";
+
+const ModernForm = props => (
+  <Fragment>
+    <HeaderApp />
+    <CurriculumGenerated propsM={props} />
+  </Fragment>
+);
+export default ModernForm;
