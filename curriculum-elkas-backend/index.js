460b5f5cdf62467f401bf0268b61c5af945a5dd3
Gabriel Lima <gabriellima.silva96@gmail.com>
2018-11-11 16:49:29 -0300

    Redirencionando o backend para a docs

--- curriculum-elkas-backend/index.js
+++ curriculum-elkas-backend/index.js
@@ -1,23 +1,27 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const app = require("express")();
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const requireDir = require("require-dir");
-const cors = require("cors");
-const swaggerUI = require("swagger-ui-express");
-const configDb = require("./config/database");
-const swaggerDocument = require("./config/swaggerDocument");
+const app = require('express')();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const requireDir = require('require-dir');
+const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const configDb = require('./config/database');
+const swaggerDocument = require('./config/swaggerDocument');
 
 mongoose.connect(
   configDb.url,
-  configDb.config
+  configDb.config,
 );
 requireDir(configDb.modelPath);
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument.json));
-app.use("/api", require("./app/routes"));
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument.json));
+app.use('/api', require('./app/routes'));
+
+app.use('/', (req, res) => {
+  res.redirect('/api/docs');
+});
 
 app.listen(3000);
